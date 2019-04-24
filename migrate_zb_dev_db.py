4480f311e35f18c9eefc0ea7446eb2f36e081af0
ygl <ygl@desktop.com>
2019-04-24 13:51:36 +0800

    u

--- migrate_zb_dev_db.py
+++ migrate_zb_dev_db.py
@@ -11,8 +11,8 @@ def main(src_db, target_db):
             models.ZbDevice.Init(conn2.conn_obj)
             for row in conn1.conn_obj.execute("select id,nid,moduleid,name,cts,device_no,remark from rxg_zb_device"):
                 print(row)
-                conn2.conn_obj.execute("insert or ignore into rgw_zb_device values(?,?,?,?,?,?,?)",
-                                       (row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
+                conn2.conn_obj.execute("insert or ignore into rgw_zb_device(id,nid,moduleid,name,cts,device_no,remark) values(?,?,?,?,?,?,?)",
+                                       (row[0],row[1],row[2],row[3],row[4],row[5], row[6] if row[6] else ''))
 
 
 if __name__ == "__main__":
